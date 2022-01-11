c923c1fd5a5ebfeaab95719887ab6754d16d4419
Nishikantaray <nishikantaray1@gmail.com>
2022-01-11 19:47:06 +0530

    added html and css commit

--- index.js
+++ index.js
@@ -1,47 +1,17 @@
-// function load_text() {
-//     let v=document.getElementById("output").value;
-//     console.log(v);
-//   }
 
-//   function bold_click() {
-//     document.getElementById("output").style.fontWeight = "bold";
-//   }
 
-//   function underline_click() {
-//     document.getElementById("output").style.textDecoration = "underline";
-//   }
-
-//   function italic_click() {
-//     document.getElementById("output").style.fontStyle = "italic";
-//   }
-
-//   function normal_click() {
-//     document.getElementById("output").style.fontStyle = "normal";
-//     document.getElementById("output").style.textDecoration = "none";
-//     document.getElementById("output").style.fontWeight = "normal";
-//   }
-  
-//   let notepad = document.getElementById('output');
-//   notepad.focus();
-  
-//   notepad.addEventListener('keyup', function (event) {
-//       chrome.storage.local.set({notepad: event.target.innerHTML}, function () {
-//           console.log("Notepad updated with", event.target.innerHTML);
-//       });
-//   });
-
-  let notepad = document.getElementById('notepad');
-notepad.focus();
-
-notepad.addEventListener('keyup', function (event) {
-    chrome.storage.local.set({notepad: event.target.innerHTML}, function () {
-        console.log("Notepad updated with", event.target.innerHTML);
-    });
-});
-
-chrome.storage.local.get(['notepad', 'text', 'background'], function (data) {
-    console.log('notepad data', data);
-    notepad.innerHTML = data.notepad;
-    notepad.style.color = data.text;
-    notepad.style.backgroundColor = data.background;
-});
+ let notepad = document.getElementById('notepad');
+ notepad.focus();
+ 
+ notepad.addEventListener('keyup', function (event) {
+     chrome.storage.local.set({notepad: event.target.innerHTML}, function () {
+         console.log("Notepad updated with", event.target.innerHTML);
+     });
+ });
+ 
+ chrome.storage.local.get(['notepad', 'text', 'background'], function (data) {
+     console.log('notepad data', data);
+     notepad.innerHTML = data.notepad;
+     notepad.style.color = data.text;
+     notepad.style.backgroundColor = data.background;
+ });
