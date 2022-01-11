d72fae998d45f629163e26c470a3eabb9d998676
Nishikantaray <nishikantaray1@gmail.com>
2022-01-11 18:52:03 +0530

    added html and css commit

--- index.js
+++ index.js
@@ -1,21 +1,31 @@
-function load_text() {
-    document.getElementById("output").value = "text....";
-  }
+// function load_text() {
+//     let v=document.getElementById("output").value;
+//     console.log(v);
+//   }
 
-  function bold_click() {
-    document.getElementById("output").style.fontWeight = "bold";
-  }
+//   function bold_click() {
+//     document.getElementById("output").style.fontWeight = "bold";
+//   }
 
-  function underline_click() {
-    document.getElementById("output").style.textDecoration = "underline";
-  }
+//   function underline_click() {
+//     document.getElementById("output").style.textDecoration = "underline";
+//   }
 
-  function italic_click() {
-    document.getElementById("output").style.fontStyle = "italic";
-  }
+//   function italic_click() {
+//     document.getElementById("output").style.fontStyle = "italic";
+//   }
 
-  function normal_click() {
-    document.getElementById("output").style.fontStyle = "normal";
-    document.getElementById("output").style.textDecoration = "none";
-    document.getElementById("output").style.fontWeight = "normal";
-  }
+//   function normal_click() {
+//     document.getElementById("output").style.fontStyle = "normal";
+//     document.getElementById("output").style.textDecoration = "none";
+//     document.getElementById("output").style.fontWeight = "normal";
+//   }
+  
+  let notepad = document.getElementById('notepad');
+  notepad.focus();
+  
+  notepad.addEventListener('keyup', function (event) {
+      chrome.storage.local.set({notepad: event.target.innerHTML}, function () {
+          console.log("Notepad updated with", event.target.innerHTML);
+      });
+  });
