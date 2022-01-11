01cb84230363d256a02ea333937aa4e5425a69a5
Nishikantaray <nishikantaray1@gmail.com>
2022-01-11 18:58:30 +0530

    added html and css commit

--- index.js
+++ index.js
@@ -21,11 +21,27 @@
 //     document.getElementById("output").style.fontWeight = "normal";
 //   }
   
-  let notepad = document.getElementById('notepad');
-  notepad.focus();
+//   let notepad = document.getElementById('output');
+//   notepad.focus();
   
-  notepad.addEventListener('keyup', function (event) {
-      chrome.storage.local.set({notepad: event.target.innerHTML}, function () {
-          console.log("Notepad updated with", event.target.innerHTML);
-      });
-  });
+//   notepad.addEventListener('keyup', function (event) {
+//       chrome.storage.local.set({notepad: event.target.innerHTML}, function () {
+//           console.log("Notepad updated with", event.target.innerHTML);
+//       });
+//   });
+
+  let notepad = document.getElementById('notepad');
+notepad.focus();
+
+notepad.addEventListener('keyup', function (event) {
+    chrome.storage.local.set({notepad: event.target.innerHTML}, function () {
+        console.log("Notepad updated with", event.target.innerHTML);
+    });
+});
+
+chrome.storage.local.get(['notepad', 'text', 'background'], function (data) {
+    console.log('notepad data', data);
+    notepad.innerHTML = data.notepad;
+    notepad.style.color = data.text;
+    notepad.style.backgroundColor = data.background;
+});
